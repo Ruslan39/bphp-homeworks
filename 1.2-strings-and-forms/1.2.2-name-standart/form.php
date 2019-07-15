94687e98f818e41854d64b91c2b509e59d44128a
Ruslan Ilderkhanov <ruslan_ild@mail.ru>
2019-07-15 08:57:34 +0200

    Task 1.2.2 fixed

--- 1.2-strings-and-forms/1.2.2-name-standart/form.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/form.php
@@ -5,11 +5,8 @@ $lastName = $_POST['lastName'];
 $middleName = $_POST['middleName'];
 
 
-//$fullname = ucfirst($lastName).' '.ucfirst($firstName).' '.ucfirst($middleName);  // странно, но ведь этот способ тоже работает
-//echo "$fullname<br>";
-
 $fullname = mb_convert_case($lastName, MB_CASE_TITLE).' '.mb_convert_case($firstName, MB_CASE_TITLE).' '.mb_convert_case($middleName, MB_CASE_TITLE);
-$fio = mb_substr($lastName, 0, 1).mb_substr($firstName, 0, 1).mb_substr($middleName, 0, 1);
-$surnameAndInitials = ucfirst($lastName).' '.mb_substr($firstName, 0, 1).'.'.mb_substr($middleName, 0, 1).'.';
+$fio = mb_convert_case(mb_substr($lastName, 0, 1), MB_CASE_TITLE).mb_convert_case(mb_substr($firstName, 0, 1), MB_CASE_TITLE).mb_convert_case(mb_substr($middleName, 0, 1), MB_CASE_TITLE);
+$surnameAndInitials = mb_convert_case($lastName, MB_CASE_TITLE).' '.mb_convert_case(mb_substr($firstName, 0, 1), MB_CASE_TITLE).'.'.mb_convert_case(mb_substr($middleName, 0, 1), MB_CASE_TITLE).'.';
 
 echo "$fullname<br>$fio<br>$surnameAndInitials";
